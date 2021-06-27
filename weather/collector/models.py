f97af0f76f795bcbc83376c2c0c198b504e084c0
roberto <alexandro.ramr777@gmail.com>
2021-06-26 22:59:51 -0500

    change default value in city_weather_info field

--- weather/collector/models.py
+++ weather/collector/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class CollectionRequest(models.Model):    
     id = models.BigIntegerField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    city_weather_info = models.JSONField(default={})
+    city_weather_info = models.JSONField(default=dict)
